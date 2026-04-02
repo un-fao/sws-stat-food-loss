0b3aa8116ce1471e0832efbadff9fa71350bf66f
Matteo Terrinoni <matteo.terrinoni@gmail.com>
2026-04-02 16:51:51 +0200

    fix

--- Documentation/render.R
+++ Documentation/render.R
@@ -1,3 +1,7 @@
 
 library("quarto")
-quarto::quarto_preview("clean_test.qmd", host = "0.0.0.0", port = 4000)
+
+# Prevent quarto from trying to open a browser
+Sys.setenv(QUARTO_BROWSER = "none")
+
+quarto::quarto_preview("clean_test.qmd", host = "0.0.0.0", port = 4000, browse = FALSE)
